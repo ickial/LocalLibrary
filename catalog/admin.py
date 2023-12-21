b53af0cb8aee5bf2ba92dd9d1db039d763a6aaa3
ickial <konetskiya@gmail.com>
2023-12-21 04:22:39 +0100

    finish work with admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,10 +5,17 @@ from .models import Book, Author, Genre, BookInstance
 admin.site.register(Genre)
 
 
+class BookAdmin(admin.TabularInline):
+    model = Book
+    exclude = ['summary', 'genre']
+
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', "date_of_birth", 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookAdmin]
 
 
 class BookInstanceInline(admin.TabularInline):
@@ -23,6 +30,6 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('imprint', 'book', 'status', 'due_back')
+    list_display = ('imprint', 'book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
     fieldsets = ((None, {'fields': ('book', 'imprint', 'id')}), ('Availability', {'fields': ('status', 'due_back')}),)
