1b1f1b92be36868e4675f5dadb849aea5ad85a0e
ickial <konetskiya@gmail.com>
2024-01-05 16:06:58 +0100

    Create page ALL BOOKS

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
+
 
 def index(request):
     num_books = Book.objects.all().count()
@@ -10,5 +11,21 @@ def index(request):
     return render(
         request,
         'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
